9df586ba305b72e78f1041040541f4c71803b2aa
zhangxun459 <72784724+zhangxun459@users.noreply.github.com>
2020-10-17 21:27:39 +0800

    Update HiWorld.java
    update...

--- src/com/company/HiWorld.java
+++ src/com/company/HiWorld.java
@@ -3,6 +3,6 @@ package com.company;
 public class HiWorld {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("你好IDEA ");
+        System.out.println("你好IDEAnnnnnnnnnnnnnnnnnnnnn ");
     }
 }
