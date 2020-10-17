42fe2f8378cc72fc8b02db261bef81f3df8a6d57
Administrator <1187989907@qq.com>
2020-10-17 15:43:10 +0800

    test

--- src/com/company/HiWorld.java
+++ src/com/company/HiWorld.java
@@ -3,6 +3,6 @@ package com.company;
 public class HiWorld {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("你好 IDEA");
+        System.out.println("你好 我是我是IDEA ");
     }
 }
