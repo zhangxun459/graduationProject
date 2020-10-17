ed636a4492437811070175ca878da34fec45cc85
Administrator <1187989907@qq.com>
2020-10-17 16:07:55 +0800

    test

--- src/com/company/HiWorld.java
+++ src/com/company/HiWorld.java
@@ -3,6 +3,20 @@ package com.company;
 public class HiWorld {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("你好 我是我是IDEA ");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("hello,add");
+        System.out.println("你好IDEA ");
     }
 }
