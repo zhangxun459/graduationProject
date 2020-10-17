157cfc00a257f5969ce674df1565bd1df444a7c6
Administrator <1187989907@qq.com>
2020-10-17 16:20:46 +0800

    test

--- src/com/company/HiWorld.java
+++ src/com/company/HiWorld.java
@@ -3,20 +3,6 @@ package com.company;
 public class HiWorld {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
-        System.out.println("hello,add");
         System.out.println("你好IDEA ");
     }
 }
